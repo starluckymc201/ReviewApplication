45c9956a722c7f0df2f77a30a89adb8bd5024a35
Trung <anhtrungdinh210@gmail.com>
2023-04-01 22:35:00 +0700

    md5

--- src/main/java/com/example/RatingBackend/controller/UserController.java
+++ src/main/java/com/example/RatingBackend/controller/UserController.java
@@ -3,13 +3,14 @@ package com.example.RatingBackend.controller;
 import com.example.RatingBackend.entity.Content;
 import com.example.RatingBackend.entity.User;
 import com.example.RatingBackend.exception.ResourceNotFoundException;
+import com.example.RatingBackend.md5.MD5;
 import com.example.RatingBackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/user")
+@RequestMapping("/api/auth")
 public class UserController {
 
     @Autowired
@@ -17,6 +18,11 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user){
-        return this.userRepository.save(user);
+        int id = user.getId();
+        String email = user.getEmail();
+        String password = MD5.getMd5(user.getPassword());
+        User newUser = new User(id, email, password);
+
+        return this.userRepository.save(newUser);
     }
 }
